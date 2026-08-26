644657a7b2d4626ea16b6f9d4283fbf10dbf951b
Brian Ortega <brian.ort02@icloud.com>
2026-08-26 12:41:00 -0500

    init: added hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Basic Hello World Program
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello, I'm Brian Ortega!\n");
+  printf("Major is Computer Engineering!\n");
+
+  return 0;
+}
